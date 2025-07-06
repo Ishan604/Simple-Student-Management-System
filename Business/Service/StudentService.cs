65c35f9427d87ffbb9c232270d3e080614702dc9
Ishan Pathirana <ishanpathirana122133@gmail.com>
2025-07-06 00:03:43 -0700

    Add files via upload

---
+++ Business/Service/StudentService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp2.Business.Interface;
+using WinFormsApp2.Models;
+using WinFormsApp2.Repository.Interface;
+using WinFormsApp2.Repository.Service;
+
+namespace WinFormsApp2.Business.Service
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentRepository _repo;
+        
+
+        public StudentService(IStudentRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public void AddStudent(StudentModel student)
+        {
+            _repo.AddStudent(student); //This means addstudent method of the repository
+        }
+
+        public void DeleteStudent(int id)
+        {
+            _repo.DeleteStudent(id);
+        }
+
+        public List<StudentModel> GetStudents()
+        {
+            return _repo.GetStudents();
+        }
+
+        public void UpdateStudent(StudentModel student)
+        {
+            _repo.UpdateStudent(student);
+        }
+    }
+}

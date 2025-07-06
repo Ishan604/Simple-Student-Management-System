b3aa00a8aeeac18e6c98207d76b00499737b8c67
Ishan Pathirana <ishanpathirana122133@gmail.com>
2025-07-06 00:00:09 -0700

    Add files via upload

---
+++ WinFormsApp2/Repository/Interface/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp2.Models;
+
+namespace WinFormsApp2.Repository.Interface
+{
+    public interface IStudentRepository
+    {
+        List<StudentModel> GetStudents();
+        void AddStudent(StudentModel student);
+        void UpdateStudent(StudentModel student);
+        void DeleteStudent(int id);
+    }
+}

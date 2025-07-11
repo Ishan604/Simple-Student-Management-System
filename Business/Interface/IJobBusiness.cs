94d03978e06e804c598b63bbac6a98e446d2c51c
Ishan Pathirana <ishanpathirana122133@gmail.com>
2025-07-11 08:47:04 -0700

    Add files via upload
    add the IJobBusiness file

---
+++ Business/Interface/IJobBusiness.cs
@@ -0,0 +1,14 @@
+﻿using Shipping_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping_System.Business.Interface
+{
+    public interface IJobBusiness
+    {
+        public void AddJob(JobsModel job);
+    }
+}
